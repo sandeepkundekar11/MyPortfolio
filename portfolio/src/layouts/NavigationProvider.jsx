eb07466fe12845ebd3de5915a2210816b7f1fd15
Sandeep K <sandeep@priyarajaelectronics.com>
2025-02-11 23:30:14 +0530

    updated the code

--- portfolio/src/layouts/NavigationProvider.jsx
+++ portfolio/src/layouts/NavigationProvider.jsx
@@ -1,25 +1,30 @@
 /* eslint-disable react/prop-types */
 const NavigationProvider = ({ children }) => {
   return (
-    <div className="lg:w-[90%] w-[95%] m-auto h-full bg-black pt-2 overflow-x-hidden">
-      <nav className="w-80 m-auto bg-gray-700 rounded-2xl h-10 ">
-        <ul className="flex space-x-8">
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
-      </nav>
-      <div className="lg:flex md:flex-row-reverse w-full aside space-y-12">
-        <div className="md:w-[70%] w-full md:h-[100vh - 3.5rem] h-full overflow-y-scroll ml-12">
-          {children}
+    <div className="w-screen h-screen overflow-x-hidden flex justify-center bg-gray-900">
+      <div className="lg:w-[90%] w-[100%] m-auto h-full  pt-2">
+        <nav className="w-80 m-auto bg-gray-700 rounded-2xl h-10 ">
+          <ul className="flex space-x-8">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </nav>
+        <div className="lg:flex lg:flex-row-reverse flex-col w-full aside space-y-12">
+
+           {/* side content */}
+          <div className="md:w-[60%] w-full  h-full mt-8 m-auto  md:ml-12">
+            {children}
+          </div>
+
+
+         {/* side profile  */}
+            <div className="lg:w-96 w-[85%] lg:h-[500px] h-[400px] border m-auto bg-white rounded-2xl lg:sticky top-8 mt-8"></div>
+          </div>
         </div>
-        <aside className="lg:w-[400px] w-full flex justify-center items-center ">
-          <div className="lg:w-96 w-[85%] lg:h-[500px] h-[400px] border bg-white rounded-2xl lg:sticky top-10 bottom-56"></div>
-        </aside>
       </div>
-    </div>
   );
 };
 export default NavigationProvider;
--- portfolio/src/pages/Home.jsx
+++ portfolio/src/pages/Home.jsx
@@ -3,11 +3,13 @@ import NavigationProvider from "../layouts/NavigationProvider";
 const Home = () => {
   return (
     <NavigationProvider>
-      <div className="w-full h-screen">
-        <h1 className="text-6xl font-bold"> hello</h1>
-      </div>
-      <div className="w-full h-screen">
-        <h1 className="text-6xl font-bold"> hello</h1>
+      <div className="md:w-full w-[95%] m-auto py-2.5">
+        <div className="home">
+          <h1 className="md:text-8xl text-5xl font-bold text-white">FULLSTACK </h1>
+          <h1 className="md:text-8xl text-5xl font-bold text-gray-700">DEVELOPER</h1>
+
+          <p className="mt-6 md:text-2xl text-lg text-gray-500">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum quidem explicabo harum reiciendis distinctio architecto dignissimos obcaecati a deleniti voluptate impedit, eos similique labore laudantium asperiores voluptatibus, reprehenderit enim omnis?</p>
+        </div>
       </div>
     </NavigationProvider>
   );
