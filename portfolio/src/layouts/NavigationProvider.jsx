04371016fda89856f28a025f0c5fe558c6f3fad9
Sandeep K <sandeep@priyarajaelectronics.com>
2025-02-12 16:58:14 +0530

    updated the portfolio home page

--- portfolio/src/layouts/NavigationProvider.jsx
+++ portfolio/src/layouts/NavigationProvider.jsx
@@ -1,11 +1,19 @@
 /* eslint-disable react/prop-types */
+import { HouseIcon } from "lucide-react";
 const NavigationProvider = ({ children }) => {
   return (
     <div className="w-screen h-screen overflow-x-hidden flex justify-center bg-gray-900">
       <div className="lg:w-[90%] w-[100%] m-auto h-full  pt-2">
         <nav className="w-80 m-auto bg-gray-700 rounded-2xl h-10 ">
           <ul className="flex space-x-8">
-            <li></li>
+            <li>
+              <div
+                className="tooltip tooltip-open tooltip-bottom"
+                data-tip="hello"
+              >
+                <button className="btn">Bottom</button>
+              </div>
+            </li>
             <li></li>
             <li></li>
             <li></li>
@@ -13,18 +21,33 @@ const NavigationProvider = ({ children }) => {
           </ul>
         </nav>
         <div className="lg:flex lg:flex-row-reverse flex-col w-full aside space-y-12">
-
-           {/* side content */}
+          {/* side content */}
           <div className="md:w-[60%] w-full  h-full mt-8 m-auto  md:ml-12">
             {children}
           </div>
 
+          {/* side profile  */}
+          <div className="lg:w-96 w-[85%] lg:h-auto lg:min-h-[600px]  p-4 border m-auto bg-white rounded-2xl lg:sticky top-8 mt-8 flex flex-col justify-center items-center">
+            <div className="m-auto w-[90%]  ">
+              <img
+                className="w-72 h-72 m-auto border rounded-2xl"
+                src=""
+                alt=""
+              />
+              <h1 className="text-4xl  text-center  font-extrabold text-black mt-3.5">
+                Sandeep N kundekar
+              </h1>
 
-         {/* side profile  */}
-            <div className="lg:w-96 w-[85%] lg:h-[500px] h-[400px] border m-auto bg-white rounded-2xl lg:sticky top-8 mt-8"></div>
+              {/* description */}
+              <h1 className="mt-8 text-lg font-bold text-gray-500 text-center">
+                MERN Stack Developer | Passionate about building scalable web
+                applications
+              </h1>
+            </div>
           </div>
         </div>
       </div>
+    </div>
   );
 };
 export default NavigationProvider;
--- portfolio/src/pages/Home.jsx
+++ portfolio/src/pages/Home.jsx
@@ -1,14 +1,59 @@
 import NavigationProvider from "../layouts/NavigationProvider";
-
+import githubIcon from "../assets/git.png";
+import linkedInIcon from "../assets/linkedin-logo.png";
 const Home = () => {
   return (
     <NavigationProvider>
-      <div className="md:w-full w-[95%] m-auto py-2.5">
-        <div className="home">
-          <h1 className="md:text-8xl text-5xl font-bold text-white">FULLSTACK </h1>
-          <h1 className="md:text-8xl text-5xl font-bold text-gray-700">DEVELOPER</h1>
+      <div className="home md:w-full w-[95%] m-auto py-2.5">
+        <div>
+          <h1 className="md:text-8xl text-5xl font-bold text-white">
+            FULLSTACK
+          </h1>
+          <h1 className="md:text-8xl text-5xl font-bold text-gray-700">
+            DEVELOPER
+          </h1>
 
-          <p className="mt-6 md:text-2xl text-lg text-gray-500">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum quidem explicabo harum reiciendis distinctio architecto dignissimos obcaecati a deleniti voluptate impedit, eos similique labore laudantium asperiores voluptatibus, reprehenderit enim omnis?</p>
+          <p className="mt-6 md:text-2xl text-lg text-gray-500">
+            Hi, I&apos;m Sandeep Kundekar, a MERN Stack Developer passionate
+            about creating scalable web applications. I specialize in React.js,
+            Node.js, MongoDB, and Playwright testing.
+          </p>
+
+          {/* social media icons */}
+          <div className="mt-6">
+            <ul className="flex space-x-8">
+              <li className="cursor-pointer">
+                <a href="https://github.com/sandeepkundekar11" target="_">
+                  <img
+                    className="md:w-12 md:h-12 w-10 h-10"
+                    src={githubIcon}
+                    alt=""
+                  />
+                </a>
+                <p className="text-gray-500 font-semibold">GitHub</p>
+              </li>
+              <li className="cursor-pointer">
+                <a href="">
+                  <img
+                    className="md:w-14 md:h-14 -mt-2 w-12 h-12"
+                    src={linkedInIcon}
+                    alt=""
+                  />
+                </a>
+                <p className="text-gray-500 font-semibold">LinkedIn</p>
+              </li>
+            </ul>
+          </div>
+
+          {/* buttons */}
+          <div className="flex space-x-7 mt-10">
+            <button className="w-auto h-10 rounded-lg px-2 text-white font-medium shadow-md bg-blue-500 hover:bg-blue-600">
+              View Projects
+            </button>
+            <button className="w-auto h-10 px-2 text-white font-medium rounded-lg shadow-md bg-blue-500 hover:bg-blue-600">
+              Download Resume
+            </button>
+          </div>
         </div>
       </div>
     </NavigationProvider>
